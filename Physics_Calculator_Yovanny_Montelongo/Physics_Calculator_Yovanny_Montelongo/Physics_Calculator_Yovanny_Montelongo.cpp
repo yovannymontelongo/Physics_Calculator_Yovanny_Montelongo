e6a2bbf7af214fbea6a0ca6b692f19b195421af1
Yovanny Montelongo <yovanny.montelongo@my.tccd.edu>
2020-10-28 12:50:23 -0500

    added a basic menu system

--- Physics_Calculator_Yovanny_Montelongo/Physics_Calculator_Yovanny_Montelongo/Physics_Calculator_Yovanny_Montelongo.cpp
+++ Physics_Calculator_Yovanny_Montelongo/Physics_Calculator_Yovanny_Montelongo/Physics_Calculator_Yovanny_Montelongo.cpp
@@ -1,11 +1,58 @@
 #include <iostream>
+#include <stdlib.h>
 #include "Input_Validation_Extended.h"
+
 
 using namespace std;
 
+char menuSystem(char chosenItem) {
+
+	do {
+
+		cout << "----Submenu " << chosenItem << "----\n";
+		cout << "you chose menu " << chosenItem << "\n";
+
+		char input = 'z';
+		cout << "to exit, enter 'e' or 'E'\n";
+		chosenItem = validateChar(input);
+
+	} while (chosenItem != 'e' && chosenItem != 'E');
+
+	return chosenItem;
+}
+
 int main(){
 
+	char menuSelectionChosen = 'z';
 
+	do {
+
+		cout << "----HOME MENU----\n";
+		cout << "a) enter menu a\n";
+		cout << "b) enter menu b\n";
+
+		char input = 'z';
+		cout << "to exit, enter 'e' or 'E'\n";
+		menuSelectionChosen = validateChar(input);
+
+		switch (menuSelectionChosen) {
+			case 'a':
+				menuSystem(menuSelectionChosen);
+				break;
+			case 'b':
+				menuSystem(menuSelectionChosen);
+				break;
+			default:
+				system("cls");
+				break;
+		}
+
+
+		system("cls");
+
+	} while (menuSelectionChosen != 'e' && menuSelectionChosen != 'E');
+
+	system("cls");
 
 	return 0;
 }
